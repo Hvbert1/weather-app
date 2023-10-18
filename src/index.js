fa8690c1e9d70e62d6a57752452070416ddd2b42
Hvbert1 <henryjiafu@gmail.com>
2023-10-18 14:03:49 +1100

    create and display form

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
+import { create } from 'lodash';
 import './style.css';
 
+fetch('https://api.weatherapi.com/v1/current.json?key=3a42f2ba01104ec98ac20757231610&q=sydney')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(r) {
+        console.log(r.location.localtime);
+    })
+    .catch(function(err) {
+        
+    });
+
+function createForm() {
+    let formArea = document.createElement("div");
+    formArea.id = "formArea";
+
+    let formInput = document.createElement("input");
+    formInput.setAttribute("type", "text");
+    formInput.placeholder = "Enter city";
+    formInput.id = "formInput";
+
+    let btn = document.createElement("button");
+    btn.innerHTML = "Search";
+    btn.setAttribute("type", "submit");
+
+    formArea.append(formInput);
+    formArea.append(btn);
+
+    document.getElementById("content").appendChild(formArea);
+}
+
+createForm();
